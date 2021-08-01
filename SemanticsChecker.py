2db80593532fb0f270cc8d34fe3d83c4075e7d8a
glubs9 <jonte.fry@gmail.com>
2021-08-02 09:22:40 +1000

    created semantics checker

---
+++ SemanticsChecker.py
@@ -0,0 +1,10 @@
+#just checks that primitive functions are primitive
+def Semantics_Checker(x):
+    #split into functions
+    #if functions are primitive recursive definitions
+    #find depth of arguments
+    #find depth of outputs in function calls
+    #check depth of further calls
+    #raise exception of depth of args <= depth of outputs
+    #check that all funcction defintions have a case for a 0 in all positions
+    pass
