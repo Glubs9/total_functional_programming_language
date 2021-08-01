408892163adba2859678e09bd35c3ce95c618cb6
glubs9 <jonte.fry@gmail.com>
2021-08-01 15:21:58 +1000

    added classes

--- Executor.py
+++ Executor.py
@@ -8,24 +8,40 @@ from collections import defaultdict
 
 global_scope = defaultdict(lambda: [])
 
+class Function:
+    def __init__(self, argments, definition): #add type signature
+        self.arguments = arguments
+        self.definition = definition
+    def __str__(self):
+        return "function: " + str(argments) + " : " + str(definition)
+
+class Data:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+    def __str__(self):
+        return self.name + "[" + " ,".join(map(str, self.data)) + "]"
+
+#class Var: #i dont implement this class because it is just a tuple (maybe if readability gets bad)
+
 def construct_args(args_in):
     args = []
     for n in reversed(args_in[1:]):
-        if type(n) is not tuple: args.append(n)
+        if type(n) is not tuple: args.append(Data(n, []))
         elif n[0] != "data-constructor": raise Exception("non data constructor in argument definition")
-        else: args.append((n[0], n[1], args.pop())) #change to handle multi arg constructors
+        else: args.append(Data(n[1], [args.pop()])) #change later to handle multi arg constructors
     return args
 
 def define_functions(IC):
     for n in IC: 
-        global_scope[n[1][0][1]].append((construct_args(n[1]), n[2]))
+        global_scope[n[1][0][1]].append(Function((construct_args(n[1]), n[2])))
 
 call_stack = [] #i'm not too happy with stack being a global variable but I will change it later
 data_stack = []
 
 stdlib = {
-        "0": lambda: data_stack.append('0'), 
-        "s": lambda: data_stack.append(("data_constructor", "s", data_stack.pop()))
+        "0": lambda: data_stack.append(Data('0', [])), 
+        "s": lambda: data_stack.append(Data("s", [data_stack.pop()]))
 }
 def call_stdlib(func_name):
     stdlib[func_name]()
@@ -34,6 +50,7 @@ def call_stdlib(func_name):
     #hadcoded for now but will be fixed later
     #args is fully constructed at this point
     #check for successful unification
+#TODO: fix this
 def unify(definition, args):
     if definition == [] and args == []: return []
     if len(definition) != len(args): return False
@@ -47,9 +64,9 @@ def unify(definition, args):
 
 #not pure
 def unify_single(definition, arg):
-    if definition == "0" and arg == "0": return []
-    elif type(arg) is not tuple: return [(definition, arg)] #check for non-snytacitcal definition
-    elif definition[]
+    if definition.name == "0" and arg.name == "0": return []
+    elif arg.name != "s": return [(definition, arg)] #check for non-snytacitcal definition
+    elif definition.name == "0": return False
     else: return unify_single(definition[2], arg[2])
 
 #have to match the correct case and return the right function
@@ -67,17 +84,15 @@ def match_function(func_name, arguments):
 
 def arg_count(func_name):
     print("getting argument count with func " + str(func_name))
-    tmp = global_scope[func_name][0]
-    print("global scope first func is " + str(tmp))
-    arguments = global_scope[func_name][0][0] #assuming all function definitions have the same am of args (check later)
-    return len(arguments)
+    tmp = global_scope[func_name][0] #assuming all functions have same arity
+    return len(tmp.arguments)
 
 def call_user_defined_func(func_name):
     arguments = []
     for n in range(arg_count(func_name)):
         arguments.append(data_stack.pop())
     scope, function = match_function(func_name, arguments) #can we do this?
-    for n in function[1]:
+    for n in function.definition:
         call_stack.append((scope, n))
 
 def var_in_scope(func_name, scope):
