5bd82ec45d784d78b7c13fd212cac55857747999
glubs9 <jonte.fry@gmail.com>
2021-10-15 23:14:34 +1100

    added structural recursion

--- SemanticsChecker.py
+++ SemanticsChecker.py
@@ -6,11 +6,14 @@ from BottomChecker import Bottom_Checker
 from DepthChecker import Depth_Checker
 from TotalChecker import Total_Checker
 from PrimitiveChecker import Primitive_Checker
+from StructuralChecker import Structural_Checker
 
 def Semantics_Checker(functions): #note, called functions don't return bools, they error the program separately
     Bottom_Checker(functions)
     prim_funcs = [n for n in functions if n[1][0] == "primitive"]
     #Depth_Checker(prim_funcs)
-    #Total_Checker(prim_funcs) #might have been better to put in the executor after analysis
-    Primitive_Checker(prim_funcs)
+    #write depth checker thing that checks recursive functions
+    Structural_Checker(prim_funcs)
+    Total_Checker(prim_funcs) #might have been better to put in the executor after analysis
+    #Primitive_Checker(prim_funcs)
     return functions
