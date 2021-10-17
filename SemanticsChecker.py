192613adc1a5ec0848c4ce907f8805a5b1ab34df
glubs9 <jonte.fry@gmail.com>
2021-10-17 17:03:18 +1100

    idk updating

--- SemanticsChecker.py
+++ SemanticsChecker.py
@@ -8,13 +8,14 @@ from TotalChecker import Total_Checker
 from PrimitiveChecker import Primitive_Checker
 from StructuralChecker import Structural_Checker
 
-def Semantics_Checker(functions): #note, called functions don't return bools, they error the program separately
-    return functions #done here to skip semantics checking for dbeugging
+def Semantics_Checker(ast): #note, called functions don't return bools, they error the program separately
+    functions = [n for n in ast if n[0] == "="] #remove the data constructors
+    data = [n for n in ast if n[0] == "data"]
     Bottom_Checker(functions)
     prim_funcs = [n for n in functions if n[1][0] == "primitive"]
     #Depth_Checker(prim_funcs)
     #write depth checker thing that checks recursive functions
     Structural_Checker(prim_funcs)
-    Total_Checker(prim_funcs) #might have been better to put in the executor after analysis
+    Total_Checker(prim_funcs, ast) #might have been better to put in the executor after analysis
     #Primitive_Checker(prim_funcs)
-    return functions
+    return ast
