3cf4fc79a9614b014b337155ed75a8d45a3d66bf
glubs9 <jonte.fry@gmail.com>
2021-10-20 14:36:28 +1100

    removed old unused files

--- SemanticsChecker.py
+++ SemanticsChecker.py
@@ -12,6 +12,8 @@ from CircularChecker import Circular_Checker
 from TypeChecker import Type_Checker
 from BracketChecker import Bracket_Checker
 
+#i feel i should be able to generalize a checker and be able to write it so that I can write less
+#repeated code. probably something to do with 
 def Semantics_Checker(ast): #note, called functions don't return bools, they error the program separately
     functions = [n for n in ast if n[0] == "="] #remove the data constructors
     data = [n for n in ast if n[0] == "data"]
@@ -19,11 +21,8 @@ def Semantics_Checker(ast): #note, called functions don't return bools, they err
     Arity_Checker(functions, data)
     Bracket_Checker(functions, data)
     prim_funcs = [n for n in functions if n[1][0] == "primitive"]
-    #Depth_Checker(prim_funcs)
-    #write depth checker thing that checks recursive functions
     Structural_Checker(prim_funcs)
-    Total_Checker(prim_funcs, data) #might have been better to put in the executor after analysis
+    Total_Checker(prim_funcs, data)
     Type_Checker(prim_funcs, data)
     Circular_Checker(prim_funcs)
-    #Primitive_Checker(prim_funcs)
     return ast
